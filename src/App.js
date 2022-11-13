ddde303c284332ff9f325312ebf90fdd6343b12a
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-11-13 21:35:32 +0200

    added section dictionary

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import AreaDiagram from "./components/AreaDiagram/AreaDiagram";
+import CoeffsSection from "./components/CoeffsSection/CoeffsSection";
+import CriteriaSection from "./components/CriteriaSection/CriteriaSection";
+import MarksSection from "./components/MarksSection/MarksSection";
+import Menu from "./components/Menu/Menu";
+import ProgrammingDiagram from "./components/ProgrammingDiagram/ProgrammingDiagram";
+import QualitySection from "./components/QualitySection/QualitySection";
+import TotalDiagram from "./components/TotalDiagram/TotalDiagram";
+import UsabilityDiagram from "./components/UsabilityDiagram/UsabilityDiagram";
+import UserDiagram from "./components/UserDiagram/UserDiagram";
 
 function App() {
+  const [currentSection,setCurrentSection]=useState(null);
+  useEffect(()=>{
+    document.addEventListener("changedSection",(e)=>{
+      setCurrentSection(section[e.detail.section]);
+    });
+  })
+  const section={
+    "coeffs":<CoeffsSection/>,
+    "marks":<MarksSection/>,
+    "criteria": <CriteriaSection/>,
+    "quality": <QualitySection/>,
+    "area":<AreaDiagram/>,
+    "usability":<UsabilityDiagram/>,
+    "programming":<ProgrammingDiagram/>,
+    "users": <UserDiagram/>,
+    "total":<TotalDiagram/>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Menu/>
+      {(()=>{return currentSection})()}    </div>
   );
 }
 
